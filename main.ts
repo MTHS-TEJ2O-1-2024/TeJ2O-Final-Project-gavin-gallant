a441ddfeb884f55a04218f3e3dd2134996ba20dd
gavin-gallant <gavin.gallant@mths.ca>
2025-01-14 11:25:35 -0500

    added a password

--- main.ts
+++ main.ts
@@ -2,17 +2,16 @@
  *
  * Created by: Gavin Gallant and Fransisco Rocco Aolgeri
  * Created on: jan 2025
- * This program ...
+ * This program closes a door when your hand it within 15 cm
 */
 
 // variables
 const servoNumber1 = robotbit.Servos.S1
 basic.clearScreen()
-basic.showIcon(IconNames.Happy)
 let distanceToObject: number = 0
+let PasscodeTime: number = 0
 
 
-// forever loops
 distanceToObject = sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
@@ -20,7 +19,7 @@ distanceToObject = sonar.ping(
 )
 input.onButtonPressed(Button.A, function () {
 
-
+    // forever loops
     while (true) {
             distanceToObject = sonar.ping(
             DigitalPin.P1,
@@ -28,7 +27,7 @@ input.onButtonPressed(Button.A, function () {
             PingUnit.Centimeters
         )
 
-        if (distanceToObject < 10) {
+        if (distanceToObject < 15) {
             robotbit.Servo(servoNumber1, 30)
             basic.clearScreen()
             basic.showLeds(`
@@ -43,7 +42,19 @@ input.onButtonPressed(Button.A, function () {
             basic.clearScreen()
             basic.showIcon(IconNames.Happy)
         }
+        PasscodeTime = 10
+        if (input.buttonIsPressed(Button.B) == true) {
+            robotbit.Servo(servoNumber1, 180)
+            basic.showIcon(IconNames.Yes)
+            while(PasscodeTime <0)
+                PasscodeTime--
+                basic.showNumber(PasscodeTime)
+                basic.pause(1000)
+            break
+        }
+
     }
+
 })
 basic.showLeds(`
 . . . . .
